5016e1624c952d7d7b6eca6553e3c7c0edfc8a03
Forrest Hilton <forrestmhilton@gmail.com>
2023-07-18 23:34:41 -0500

    accedentaly omited visual settings file

---
+++ manim_lamination_builder/visual_settings.py
@@ -0,0 +1,33 @@
+from manim.utils.color import Colors
+
+colors_list = [
+    Colors.pure_red,
+    Colors.pure_green,
+    Colors.pure_blue,
+    Colors.yellow,
+    Colors.purple,
+]
+
+def get_color(i:int):
+    if i >= len(colors_list):
+        return Colors.black
+    return colors_list[i]
+
+class VisualSettings:
+    point_color: Colors
+    stroke_color: Colors
+    polygon_color: Colors
+    point_size: float
+    stroke_width: float
+
+    def __init__(
+        self, point_color = Colors.red, stroke_color = Colors.black, polygon_color = Colors.blue_c, point_size = 0.04, stroke_width = 4):
+        self.point_color = point_color
+        self.stroke_color = stroke_color # TODO
+        self.polygon_color = polygon_color 
+        self.point_size = point_size
+        self.stroke_width = stroke_width# TODO
+
+    @staticmethod
+    def default():
+        return VisualSettings()
