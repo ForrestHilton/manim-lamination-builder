6c06263d1bb834cb5bcf447e3f8ae9a8cb3ddc85
Forrest Hilton <forrestmhilton@gmail.com>
2025-09-29 18:14:34 -0500

    add a test and a TODO list

--- tests/orbits_test.py
+++ tests/orbits_test.py
@@ -1,18 +1,59 @@
-from manim_lamination_builder import NaryFraction
-from manim_lamination_builder.orbits import goldbergOrbit
+import itertools
+import random
+from math import gcd
 
-def test_deploymentSequence():
+from manim_lamination_builder import goldbergOrbit
+from manim_lamination_builder.constructions import sigma
 
-    assert goldbergOrbit([3],[1,3]).to_string() == "_001"
-    assert goldbergOrbit([2,3],[1,3]).to_string() == "_002"
-    assert goldbergOrbit([1,3],[1,3]).to_string() == "_012"
-    
-def test_rotationNumber():
-    
-    assert goldbergOrbit([2,3],[1,3]).to_string() == "_002"
-    assert goldbergOrbit([2,3],[2,3]).to_string() == "_021"
 
-def test_compositeRotationNumber():
-    
-    assert goldbergOrbit([2,4],[2,4]).to_string() == "_02"
-    assert goldbergOrbit([4,4],[2,4]).to_string() == "_01"
+def test_deployment_sequence():
+    assert goldbergOrbit([3], [1, 3]).to_string() == "_001"
+    assert goldbergOrbit([2, 3], [1, 3]).to_string() == "_002"
+    assert goldbergOrbit([1, 3], [1, 3]).to_string() == "_012"
+
+
+def test_rotation_number():
+    assert goldbergOrbit([2, 3], [1, 3]).to_string() == "_002"
+    assert goldbergOrbit([2, 3], [2, 3]).to_string() == "_021"
+
+
+def test_composite_rotation_number():
+    assert goldbergOrbit([2, 4], [2, 4]).to_string() == "_02"
+    assert goldbergOrbit([4, 4], [2, 4]).to_string() == "_01"
+
+
+def random_partition(total, slots):
+    # Pick slots-1 cut-points in 0..total (inclusive), sort, and use differences
+    cuts = sorted([0] + [random.randint(0, total) for _ in range(slots - 1)] + [total])
+    return [cuts[i + 1] - cuts[i] for i in range(slots)]
+
+
+def test_goldberg_random_is_correct():
+    for den in range(1, 5):
+        for num in range(1, den):
+            if gcd(num, den) != 1:
+                break
+            for d in range(2, 4):
+                rand_list = random_partition(den, d - 1)
+                deployment = list(itertools.accumulate(rand_list))
+                point = goldbergOrbit(deployment, [num, den])
+                pair = "goldbergOrbit({},[{},{}]) -> {}".format(
+                    deployment, num, den, point
+                )
+                iterate = point
+                orbit = []
+                for _ in range(len(point.repeating)):
+                    orbit.append(iterate.to_float())
+                    iterate = sigma(iterate)
+                for i, quota in enumerate(deployment):
+                    assert (
+                        len([x for x in orbit if x < (i + 1) / (d - 1)]) == quota
+                    ), "goldbergOrbit does not match the deployment sequence: {}".format(
+                        pair
+                    )
+
+
+# TODO: Should the goldberg orbit return the least moment of the orbit?
+# TODO: can the test above test if the rotation number is respected?
+# TODO: can the test above be extended for gcd != 1
+# TODO: Should orbits.py use the python convention for variable names?
