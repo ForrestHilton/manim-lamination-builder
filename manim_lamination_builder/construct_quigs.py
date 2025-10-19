42d7973bb98ea4394e6e61d520dda8de6e448333
Forrest Hilton <forrestmhilton@gmail.com>
2025-10-19 09:49:06 -0500

    short and long quigs mostly working

--- manim_lamination_builder/construct_quigs.py
+++ manim_lamination_builder/construct_quigs.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Callable, Iterable
 
 from manim import Scene, tempconfig
 
@@ -47,38 +47,44 @@ def build_quig(insertion_point: NaryFraction) -> LeafLamination:
     return LeafLamination(points=[], leafs=leaves, degree=3)
 
 
-periodic_points = set(
-    [
-        NaryFraction(exact=(), repeating=tuple(str), degree=2)
-        for str in cumulative_base_strings(9, 2)
-    ]
-)
+def periodic_points(max_period=6):
+    return set(
+        [
+            NaryFraction(exact=(), repeating=tuple(str), degree=2)
+            for str in cumulative_base_strings(max_period, 2)
+        ]
+    )
 
-pre_iterates_of_zero = set(
-    [
-        NaryFraction(exact=tuple(str), repeating=(), degree=2)
-        for str in cumulative_base_strings(6, 2)
-    ]
-)
+
+def pre_iterates_of_zero():
+    return set(
+        [
+            NaryFraction(exact=tuple(str), repeating=(), degree=2)
+            for str in cumulative_base_strings(6, 2)
+        ]
+    )
 
 
 class Quigs(Scene):
     "displays all the quigs in sorted order using the provided insertion points"
 
-    def __init__(self, points: Iterable[Angle]):
+    def __init__(
+        self, points: Iterable[Angle], quig_function: Callable[[Angle], LeafLamination]
+    ):
         self.points = points
+        self.quig_function = quig_function
         super().__init__()
 
     def construct(self):
         for point in sorted(self.points):
-            self.add(build_quig(point).build(2))
+            self.add(self.quig_function(point).build(2))
             self.wait(0.1)
             self.clear()
 
 
 def generable_co_majors() -> LeafLamination:
     leaves = []
-    for insertion_point in sorted(periodic_points):
+    for insertion_point in sorted(periodic_points(9)):
         sibling = insertion_point.other_sibling()[0]
         leaves.append(Psi(sibling, insertion_point))
     return LeafLamination(points=[], leafs=leaves, degree=3)
@@ -94,10 +100,47 @@ def duplicate_by_fixed_points(lam: LeafLamination) -> LeafLamination:
     return LeafLamination(points=[], leafs=leaves, degree=lam.degree)
 
 
+def Psi_lam(
+    lam: LeafLamination, insertion_point: Angle, additional_leaves=False
+) -> LeafLamination:
+    leaves = []
+    for leaf in lam.leafs:
+        leaves.append(
+            Chord(
+                psi(leaf.min, insertion_point),
+                psi(leaf.max, insertion_point, lesser=False),
+            )
+        )
+    assert not additional_leaves
+    return LeafLamination(points=[], leafs=leaves, degree=lam.degree)
+
+
+def short_quig(insertion_point: NaryFraction) -> LeafLamination:
+    "takes a quadratic insertion point and performs two insertions"
+    assert insertion_point.degree == 2
+    cubic = build_quig(insertion_point)
+    new_insertion_point = psi(insertion_point, insertion_point)
+    return Psi_lam(cubic, new_insertion_point)
+
+
+def long_quig(insertion_point: NaryFraction) -> LeafLamination:
+    "takes a quadratic insertion point and performs two insertions"
+    assert insertion_point.degree == 2
+    cubic = build_quig(insertion_point)
+    new_insertion_point = psi(insertion_point.other_sibling()[0], insertion_point)
+    return Psi_lam(
+        cubic, new_insertion_point, additional_leaves=not insertion_point.periodic()
+    )
+
+
 if __name__ == "__main__":
+    with tempconfig({"quality": "high_quality", "preview": True}):
+        Quigs(periodic_points(5), long_quig).render()
     with tempconfig(
         {"quality": "fourk_quality", "preview": True}  # , "background_color": WHITE
     ):
+        pass
         # Main([build_quig(NaryFraction.from_string(2, "_01"))]).render()
-        Main([duplicate_by_fixed_points(generable_co_majors())]).render()
+        # Main([duplicate_by_fixed_points(generable_co_majors())]).render()
         # Quigs(pre_iterates_of_zero).render()
+        # Main([short_quig(NaryFraction.from_string(2, "_01"))]).render()
