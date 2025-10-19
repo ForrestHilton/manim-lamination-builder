c2703bc3e4cc5e3f68872d4edd6c6736546d181a
Forrest Hilton <forrestmhilton@gmail.com>
2025-10-19 09:58:47 -0500

    reorganize file

--- manim_lamination_builder/construct_quigs.py
+++ manim_lamination_builder/construct_quigs.py
@@ -9,64 +9,9 @@ from manim_lamination_builder.malaugh import Psi, psi
 from manim_lamination_builder.orbits import Orbit
 from manim_lamination_builder.points import Angle, NaryFraction
 
-# insert at _10 in degree 2 to make a quig in degree 3.
 
-
-def base_strings(length, radix):  # TODO: convert to an iteraitor of narryfractions
-    if radix < 2:
-        raise ValueError("radix must be at least 2")
-    for i in range(radix**length):
-        n = i
-        chars = []
-        for _ in range(length):
-            chars.append(n % radix)
-            n //= radix
-        yield tuple(reversed(chars))
-
-
-def cumulative_base_strings(max_length, radix):
-    for length in range(max_length):
-        for string in base_strings(length, radix):
-            yield string
-
-
-def build_quig(insertion_point: NaryFraction) -> LeafLamination:
-    eventual_preimages = []
-    for exact in cumulative_base_strings(8, 2):
-        eventual_preimages.append(
-            NaryFraction(
-                exact=exact + insertion_point.exact,
-                repeating=insertion_point.repeating,
-                degree=2,
-            )
-        )
-
-    leaves = []
-    for p in eventual_preimages:
-        leaves.append(Psi(p, insertion_point))
-    return LeafLamination(points=[], leafs=leaves, degree=3)
-
-
-def periodic_points(max_period=6):
-    return set(
-        [
-            NaryFraction(exact=(), repeating=tuple(str), degree=2)
-            for str in cumulative_base_strings(max_period, 2)
-        ]
-    )
-
-
-def pre_iterates_of_zero():
-    return set(
-        [
-            NaryFraction(exact=tuple(str), repeating=(), degree=2)
-            for str in cumulative_base_strings(6, 2)
-        ]
-    )
-
-
-class Quigs(Scene):
-    "displays all the quigs in sorted order using the provided insertion points"
+class GenerateLams(Scene):
+    "displays all the laminations in sorted order generated from the points using the function provided"
 
     def __init__(
         self, points: Iterable[Angle], quig_function: Callable[[Angle], LeafLamination]
@@ -82,12 +27,32 @@ class Quigs(Scene):
             self.clear()
 
 
-def generable_co_majors() -> LeafLamination:
-    leaves = []
-    for insertion_point in sorted(periodic_points(9)):
-        sibling = insertion_point.other_sibling()[0]
-        leaves.append(Psi(sibling, insertion_point))
-    return LeafLamination(points=[], leafs=leaves, degree=3)
+def base_strings(length, radix):
+    if radix < 2:
+        raise ValueError("radix must be at least 2")
+    for i in range(radix**length):
+        n = i
+        chars = []
+        for _ in range(length):
+            chars.append(n % radix)
+            n //= radix
+        yield tuple(reversed(chars))
+
+
+def periodic_points(max_period=6):
+    rets = cumulative_base_strings(max_period, 2)
+    return set([NaryFraction(exact=(), repeating=tuple(str), degree=2) for str in rets])
+
+
+def pre_iterates_of_zero():
+    strs = cumulative_base_strings(6, 2)
+    return set([NaryFraction(exact=tuple(str), repeating=(), degree=2) for str in strs])
+
+
+def cumulative_base_strings(max_length, radix):
+    for length in range(max_length):
+        for string in base_strings(length, radix):
+            yield string
 
 
 def duplicate_by_fixed_points(lam: LeafLamination) -> LeafLamination:
@@ -115,6 +80,31 @@ def Psi_lam(
     return LeafLamination(points=[], leafs=leaves, degree=lam.degree)
 
 
+def build_quig(insertion_point: NaryFraction) -> LeafLamination:
+    eventual_preimages = []
+    for exact in cumulative_base_strings(8, 2):
+        eventual_preimages.append(
+            NaryFraction(
+                exact=exact + insertion_point.exact,
+                repeating=insertion_point.repeating,
+                degree=2,
+            )
+        )
+
+    leaves = []
+    for p in eventual_preimages:
+        leaves.append(Psi(p, insertion_point))
+    return LeafLamination(points=[], leafs=leaves, degree=3)
+
+
+def generable_co_majors() -> LeafLamination:
+    leaves = []
+    for insertion_point in sorted(periodic_points(9)):
+        sibling = insertion_point.other_sibling()[0]
+        leaves.append(Psi(sibling, insertion_point))
+    return LeafLamination(points=[], leafs=leaves, degree=3)
+
+
 def short_quig(insertion_point: NaryFraction) -> LeafLamination:
     "takes a quadratic insertion point and performs two insertions"
     assert insertion_point.degree == 2
@@ -135,7 +125,7 @@ def long_quig(insertion_point: NaryFraction) -> LeafLamination:
 
 if __name__ == "__main__":
     with tempconfig({"quality": "high_quality", "preview": True}):
-        Quigs(periodic_points(5), long_quig).render()
+        GenerateLams(periodic_points(5), long_quig).render()
     with tempconfig(
         {"quality": "fourk_quality", "preview": True}  # , "background_color": WHITE
     ):
